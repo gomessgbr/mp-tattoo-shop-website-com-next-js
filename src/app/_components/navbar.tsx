22d62e76b1327e3bd460e855e1e47c9ea00121f4
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-12-06 12:07:46 -0300

    wip: navbar

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -7,31 +7,25 @@ import { FaWhatsapp } from "react-icons/fa";
 
 export default function Navbar() {
   return (
-    <header className="min-w-full grid grid-cols-2">
-      <div>
-        <Link href={"/"}>
-          <Image alt="logo da página" src={logo} />
-        </Link>
-      </div>
-      <nav className="grid items-center ">
-        <ul className="flex gap-10 text-headings font-londrina font-bold text-2xl ">
-          <li>
-            <Link href={"#"}>Serviços</Link>
-          </li>
-          <li>
-            <Link href={"#"}>Sobre mim</Link>
-          </li>
-          <li>
-            <Link href={"#"}>Contato</Link>
-          </li>
-        </ul>
-        <div className="flex gap-1 justify-end">
-          <FaWhatsapp />
-          <span className="font-sans font-medium text-muted">
-            (+55 11) 99999 9999
+    <nav className="container flex justify-between font-londrina py-8 ">
+      <Link href={"/"}>
+        <Image alt="logo da página" src={logo} />
+      </Link>
+      <ul className="flex gap-6 text-headings font-londrina font-bold text-3xl ">
+        <li>
+          <Link href={"#"}>Serviços</Link>
+        </li>
+        <li>
+          <Link href={"#"}>Sobre mim</Link>
+        </li>
+        <li className="text-right">
+          <Link href={"#"}>Contato</Link> <br />
+          <span className="text-xs font-extralight flex gap-1 text-muted items-center">
+            {" "}
+            <FaWhatsapp /> (+55 11) 99999 9999
           </span>
-        </div>
-      </nav>
-    </header>
+        </li>
+      </ul>
+    </nav>
   );
 }
