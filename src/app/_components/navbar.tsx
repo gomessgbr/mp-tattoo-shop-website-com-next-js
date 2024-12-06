0090e3e3af181dc6948cc245bc9994f3bc53455d
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-12-06 12:33:25 -0300

    feat: navbar

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -1,29 +1,39 @@
-import React from "react";
+"use client";
 import Image from "next/image";
 import logo from "../_assets/logo.svg";
-
+import React from "react";
 import Link from "next/link";
 import { FaWhatsapp } from "react-icons/fa";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
+  const pathName = usePathname();
+
+  const isActive = (currentPath: string, pathname: string) => {
+    if (currentPath.includes(pathname)) return "underline";
+  };
   return (
-    <nav className="container flex justify-between font-londrina py-8 ">
+    <nav className="container flex justify-between py-8 ">
       <Link href={"/"}>
         <Image alt="logo da página" src={logo} />
       </Link>
-      <ul className="flex gap-6 text-headings font-londrina font-bold text-3xl ">
-        <li>
-          <Link href={"#"}>Serviços</Link>
+      <ul className="flex gap-6 text-headings font-londrina font-bold text-3xl">
+        <li className={`${isActive(pathName, "services")}`}>
+          <Link href={"/services"}>Serviços</Link>
         </li>
-        <li>
-          <Link href={"#"}>Sobre mim</Link>
+        <li className={`${isActive(pathName, "about")}`}>
+          <Link href={"/about"}>Sobre mim</Link>
         </li>
-        <li className="text-right">
-          <Link href={"#"}>Contato</Link> <br />
-          <span className="text-xs font-extralight flex gap-1 text-muted items-center">
-            {" "}
-            <FaWhatsapp /> (+55 11) 99999 9999
-          </span>
+        <li className={`text-right flex gap-1`}>
+          <Link href={"/contact"}>
+            <span className={`${isActive(pathName, "contact")}`}>
+              Contato <br />
+            </span>
+
+            <span className="font-sans text-xs font-extralight flex gap-1 text-muted items-center">
+              <FaWhatsapp /> (+55 11) 99999 9999
+            </span>
+          </Link>
         </li>
       </ul>
     </nav>
