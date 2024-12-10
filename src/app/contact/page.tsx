dc1d7f961d83d5f2bdc20ec01489a82c2fbd4ac3
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-12-10 10:50:10 -0300

    feat: whatsapp button

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import React from "react";
 import contactUs from "../_assets/contactUs.svg";
 import { FaWhatsapp } from "react-icons/fa6";
+import Link from "next/link";
 export default function Contact() {
   return (
     <main className="grid lg:grid-cols-2 lg: grid-rows-1 place-items-center ">
@@ -13,6 +14,10 @@ export default function Contact() {
           Clique no botão abaixo e envie uma mensagem. <br />
           Vamos conversar!
         </p>
+        <Link
+          target="_blank"
+          href="https://wa.me//551199999999?text=Ol%C3%A1%20Sandro%2C%20tenho%20interesse%20em%20fazer%20uma%20tattoo%20com%20voc%C3%AA%2C%20quais%20s%C3%A3o%20os%20pr%C3%B3ximos%20passos"
+        ></Link>
         <button
           type="button"
           className="flex place-items-center gap-7 bg-zinc-700 py-5 px-7 rounded-md"
